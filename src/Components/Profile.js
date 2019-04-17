e0a65b8e5f5df2cb57b0b8aaf677ebc7b56bafe3
admin <admin@admins-MBP.Dlink>
2019-04-17 12:21:40 +0530

    All plugin installations

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, StyleSheet,Button } from 'react-native';
+import { View, StyleSheet,Button, Dimensions, Text } from 'react-native';
 
 import Camera from 'react-native-camera';
 import { RNCamera, FaceDetector } from 'react-native-camera';
 
 
-export default class Home extends Component {
+export default class Profile extends Component {
     constructor(props) {
         super(props)
     }
@@ -16,49 +16,46 @@ export default class Home extends Component {
 
     takePicture() {
         this.camera
-            .capture()
-            .then((data) => console.log(data))
-            .catch(err => console.error(err));
-    }
-
-    render() {
-        return (
-            <View >
-                <Camera
-                    ref={cam => { this.camera = cam }}
-                    style={styles.preview}
-                    aspect={Camera.constants.Aspect.fill}
-                >
-
-                    <Button
-                        title='ClickToCapture'
-                        style={styles.capture} onPress={this.takePicture.bind(this)}
-                        onPress={this.login}
-                    />
-                </Camera>
-
-
+          .capture()
+          .then((data) => console.log(data))
+          .catch(err => console.error(err));
+      }
+    render(){
+        return(
+            <View style={styles.container}>
+            <Camera
+            ref={cam => {this.camera = cam}}
+            style={styles.preview}
+            aspect={Camera.constants.Aspect.fill}
+            >
+            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+            [CAPTURE]
+            </Text>
+            </Camera>
             </View>
-
-        )
+        );
     }
-}
+};
+
+
 const styles = StyleSheet.create({
-
-    
-//     preview: {
-//         flex: 1,
-//         justifyContent: 'flex-end',
-//         alignItems: 'center',
-//         height: Dimensions.get('window').height,
-//         width: Dimensions.get('window').width},
-//         capture: {
-//           flex: 0,
-//           backgroundColor: '#fff',
-//           borderRadius: 5,
-//           color: '#000',
-//           padding: 10,
-//           margin: 40  
-//         }
-
- });
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+},
+    capture: {
+      flex: 0,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      color: '#000',
+      padding: 10,
+      margin: 40  
+    }
+});
